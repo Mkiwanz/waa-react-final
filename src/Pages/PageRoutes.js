80ed7fcf2a767744eb1159a3aaf33636840d12ba
Mkiwanz <117238114+Mkiwanz@users.noreply.github.com>
2023-02-06 15:39:01 -0600

    add Material UI

--- src/Pages/PageRoutes.js
+++ src/Pages/PageRoutes.js
@@ -1,12 +1,14 @@
 import { BrowserRouter } from "react-router-dom";
 import { Route, Routes, Navigate } from "react-router";
 import HomePage from "./HomePage";
+import ResponsiveAppBar from "../Components/AppBar/AppBar";
 
 const PageRoutes = () => {
   return (
     <BrowserRouter>
+      <ResponsiveAppBar />
       <Routes>
-        <Route path="/" element={<HomePage/>} />
+        <Route path="/" element={<HomePage />} />
       </Routes>
     </BrowserRouter>
   );
