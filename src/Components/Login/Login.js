959db007b87fbeb853b55de79a0ca0b0653ad5e3
Baha-amayreh <74584847+Baha-amayreh@users.noreply.github.com>
2023-02-07 14:50:37 -0600

    login in by baha

---
+++ src/Components/Login/Login.js
@@ -0,0 +1,41 @@
+import axios from "axios";
+import { useRef, useState } from "react";
+import "./Login.css";
+import { useNavigate } from "react-router";
+const Login = () => {
+    const userFrom = useRef();
+    const [emailState, setEmailState] = useState('');
+    const [passwordState, setPasswordState] = useState('');
+
+    function submitHandler(e) {
+        const form = userFrom.current;
+        e.preventDefault();
+        if (emailState.length !== 0 && passwordState.length !== 0) {
+            //login in function here
+        } else {
+            alert('All fields are required');
+        }
+    }//end of submitHandler function
+
+    return (
+        <form ref={userFrom} className="form1">
+            <fieldset>
+                <legend>Create a professional account</legend>
+                Email address
+                <br />
+                <input className="input1" name="email" type="email" required="required" placeholder="Enter your email address"
+                    onChange={e => setEmailState(e.target.value)}
+                />
+                <br />
+                Password
+                <br />
+                <input className="input1" name="password" required="required" type="password" placeholder="Enter your password" defaultValue=''
+                    onChange={e => setPasswordState(e.target.value)}
+                />
+                <br />
+                <input type="submit" id="button" onClick={submitHandler} />
+            </fieldset>
+        </form>
+    )
+}
+export default Login;
